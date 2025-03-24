700ed12a793bb4109589fbd1e6dff73d6df4e443
pedrolcadete <157548497+pedrolcadete@users.noreply.github.com>
2025-03-23 21:18:10 -0300

    Add files via upload

---
+++ adv_project.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+df = pd.read_excel(r"C:\Users\Pedro Lucas\Desktop\projeto_data\dados_escritorio.xlsx"r"")
+
+coluna_adv = list(df['Advogado'])
+advogados = list(dict.fromkeys(coluna_adv))
+
+# Quantos processos cada advogado tem ativo, perdido e ganho
+
+list_ativos = []
+list_perdidos = []
+list_ganhos = []
+
+status = [list_ativos, list_perdidos, list_ganhos]
+
+for advogado in advogados:
+    proc_status = ['Ativo', 'Perdido', 'Ganho']
+    df_adv = df.loc[df['Advogado'] == advogado]
+    cont = 0
+
+    for proc in proc_status:
+        list_status = list(df_adv['Status'])
+        count_status = list_status.count(proc)
+        st = status[cont]
+        st.append(count_status)
+        cont += 1
+
+# Plotar gráfico de barras
+
+x = np.arange(len(advogados))
+
+largura = 0.25
+
+plt.bar(x - largura, list_ativos, label=advogados, width=largura, color='skyblue')
+plt.bar(x, list_perdidos, label=advogados, width=largura, color='salmon')
+plt.bar(x + largura, list_ganhos, label=advogados, width=largura, color='lightgreen')
+
+legend_handles = [
+    Line2D([0], [0], marker='o', color='w', markerfacecolor='skyblue', markersize=10, label='Ativos'),
+    Line2D([0], [0], marker='o', color='w', markerfacecolor='salmon', markersize=10, label='Perdidos'),
+    Line2D([0], [0], marker='o', color='w', markerfacecolor='lightgreen', markersize=10, label='Ganhos')
+]
+
+plt.legend(handles=legend_handles)
+
+plt.xlabel('Advogados')
+plt.ylabel('Quantidade de casos')
+plt.title(('Quantos Processos Ativos, Perdidos e Ganhadas Cada Advogado Possui').upper())
+plt.xticks(x - 0.7, advogados, rotation=28)
+plt.yticks(np.arange(0, 5, 1))
+
+plt.grid(True, axis='y', linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.show()
---
+++ adv_project_finaceiro.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_excel(r"C:\Users\Pedro Lucas\Desktop\projeto_data\dados_escritorio.xlsx"r"")
+df_cut = df[['Advogado', 'Status', 'Receita (R$)', 'Custo (R$)']]
+df_finc = df_cut.loc[df_cut['Status'] != 'Ativo']
+
+col_adv = list(df['Advogado'])
+list_adv = list(dict.fromkeys(col_adv))
+
+# Lucro por médio por caso
+
+list_lucro_med = {}
+
+for adv in list_adv:
+    df_lucro = df_finc.loc[df_finc['Advogado'] == adv]
+    col_lucro = list(df_lucro['Receita (R$)'] - df_lucro['Custo (R$)'])
+
+    divisor = len(col_lucro)
+    if divisor == 0:
+        divisor = 1
+    lucro = sum(col_lucro)
+    lucro_med = lucro / divisor
+    list_lucro_med[adv] = lucro_med
+
+# Gerar tabela (arquivo xlsx) com a relação entre advogados e lucro
+
+df_x = pd.DataFrame(list(list_lucro_med.items()), columns=['Advogados', 'Lucro Médio (R$)'])
+df_x.to_excel('lucro_medio_por_advogado.xlsx', index=False)
